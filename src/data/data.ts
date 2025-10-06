e3507b72b23f33645b6fe3ec2fb1932bcfe30d45
Ben Rogers <benrogers1299@outlook.com>
2025-10-07 08:48:06 +1000

    Update data

--- src/data/data.ts
+++ src/data/data.ts
@@ -10,7 +10,7 @@ export const personalData = {
   description:
     "Full Stack Software Engineer at Humanforce. I build web applications with a focus on performance and scalability.",
   summary:
-    "I'm a Software Engineer with **4 years of experience**, currently working in the Platform Engineering team at Humanforce. I specialise in building scalable systems, improving developer experience, and solving platform-wide productivity and performance challenges across products like intelliHR, Thrive, and Workforce Management - serving **600,000+ users**. I hold a **Bachelor's degree in Computer Science** from Queensland University of Technology. I enjoy exploring new tech in my free time.",
+    "I'm a Software Engineer with **4 years of experience**, currently working in the Platform Engineering team at Humanforce. I specialise in building scalable systems, and solving platform-wide productivity and performance challenges across products like Humanforce HR, and Workforce Management - used by **600,000+ users**. I hold a **Bachelor's degree in Computer Science** from Queensland University of Technology. I enjoy exploring new tech in my free time.",
   avatarUrl: "/me.png",
 } as const;
 
