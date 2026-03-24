418462a7c6c44a62032dc1c4664f087fc1ae50be
Ben Rogers <benrogers1299@outlook.com>
2026-03-24 22:36:58 +0800

    Increase font size of blog posts

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -31,12 +31,14 @@ export default async function BlogPage() {
         .map((post, id) => (
           <BlurFade delay={BLUR_FADE_DELAY * 2 + id * 0.05} key={post.slug}>
             <Link
-              className="flex flex-col space-y-1 mb-4"
+              className="flex flex-col space-y-2 mb-8 hover:opacity-70 transition-opacity"
               href={`/blog/${post.slug}`}
             >
               <div className="w-full flex flex-col">
-                <p className="tracking-tight">{post.metadata.title}</p>
-                <p className="h-6 text-xs text-muted-foreground">
+                <p className="text-base md:text-lg font-medium tracking-tight">
+                  {post.metadata.title}
+                </p>
+                <p className="text-xs md:text-sm text-muted-foreground">
                   {formatDate(post.metadata.publishedAt)}
                 </p>
               </div>
