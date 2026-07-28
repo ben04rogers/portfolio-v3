a6c10d8d3072e27ba7784bc0f3c4eee264eeb11a
Ben Rogers <benrogers1299@outlook.com>
2026-07-28 19:49:43 +0800

    Fix spacing on home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -69,7 +69,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body
         className={cn(
-          "min-h-screen bg-background font-sans antialiased max-w-2xl mx-auto py-12 sm:py-24 px-6",
+          "min-h-screen bg-background font-sans antialiased max-w-2xl mx-auto pt-4 sm:pt-6 pb-24 sm:pb-32 px-6",
           fontSans.variable,
         )}
       >
